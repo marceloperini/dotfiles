0520522223c83a5e252cf72417ce5a5944226f2e
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2018-02-01 23:04:45 -0200

    add new alias

--- alias.zsh
+++ alias.zsh
@@ -10,14 +10,19 @@ alias ev='vim ~/.vimrc'
 alias et='vim ~/.tmux.conf'
 alias ez='vim ~/.zshrc'
 
+# update source zshrc
+alias sz='source ~/.zshrc'
+
 # to use pgUp and pgDn in irssi
 alias irssi='TERM=screen irssi'
 
 alias rake='noglob rake'
 
+# postgres alias
 alias postgres_start='sudo service postgresql start'
 alias postgres_stop='sudo service postgresql stop'
 
+# git alias
 alias pull_master='git pull origin master'
 alias pull_develop='git pull origin develop'
 alias push_master='git push origin master'
