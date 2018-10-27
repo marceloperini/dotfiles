486d2455474c5359e38e0e0afea5c8b08f9a9f66
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2018-10-27 17:43:08 -0300

    configuring polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,20 +1,11 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 # Terminate already running bar instances
-# killall -q polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar base &
-
-sleep .5
-
-if ! pgrep -x polybar; then
-	polybar base &
-else
-	pkill -USR1 polybar
-fi
-
-echo "Bars launched..."
+MONITOR=eDP1 polybar base &
+MONITOR=HDMI1 polybar base &
+MONITOR=DP2 polybar master &
