20d7ba6794006532046a13ba044712d8cb545e46
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2019-05-25 14:53:37 -0300

    configure polybar launch for laptop

--- config/polybar/launch_laptop.sh
+++ config/polybar/launch_laptop.sh
@@ -6,5 +6,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR=DVI-I-1 polybar base &
-MONITOR=DVI-D-0 polybar master &
+MONITOR=eDP1 polybar base &
