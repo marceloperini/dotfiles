515a40e94836c242699855986916f94e4d19e356
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2019-06-03 15:37:31 -0300

    add xrandir launch_pc

---
+++ config/polybar/launch_pc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+MONITOR=DVI-D-0 polybar base &
+MONITOR=HDMI-0 polybar master &
