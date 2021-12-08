152d78d97092f016811ea1baec64541d6d9bb303
Marcelo Perini Veloso <marcelo.perini.veloso@pm.me>
2021-12-08 15:53:01 -0300

    update zsh alias

--- alias.zsh
+++ alias.zsh
@@ -1,9 +1,5 @@
-# aliases for Tmux
-alias tmux='tmux -2'
-alias ta='tmux attach -t'
-alias tnew='tmux new -s'
-alias tls='tmux ls'
-alias tkill='tmux kill-session -t'
+# Gotop with perfect theme
+alias gtop='gotop --color=monokai'
 
 # convenience aliases for editing configs
 alias ev='vim ~/.vimrc'
@@ -13,27 +9,29 @@ alias ez='vim ~/.zshrc'
 # update source zshrc
 alias sz='source ~/.zshrc'
 
-# to use pgUp and pgDn in irssi
-alias irssi='TERM=screen irssi'
+# TMUX alias
+alias tmux='tmux -2'
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
 
-alias rake='noglob rake'
 # postgres alias
 alias postgres_start='sudo service postgresql start'
 alias postgres_stop='sudo service postgresql stop'
 
-# Gotop with perfect theme
-alias gtop='gotop --color=monokai'
+# ruby alias
+alias be='bundle exec'
+alias rspec='be rspec'
+alias rails='be rails'
+alias rake='be rake'
 
 # git alias
-alias add_all='git add --all'
-alias commit='git commit -m'
-alias status='git status'
-alias checkout_master='git checkout master'
-alias checkout_dev='git checkout dev'
-alias pull_origin='git pull origin'
-alias pull_master='git pull origin master'
-alias pull_dev='git pull origin dev'
-alias push_master='git push origin master'
-alias push_dev='git push origin dev'
-alias fetch_origin='git fetch origin'
-alias gittree='git log --graph --pretty=oneline --abbrev-commit'
+alias gstatus='git status'
+alias gall='git add --all'
+alias gm='git commit -m'
+alias gcheckout='git checkout'
+alias gpull='git pull origin'
+alias gpush='git push origin'
+alias gfetch='git fetch origin'
+alias gtree='git log --graph --pretty=oneline --abbrev-commit'
