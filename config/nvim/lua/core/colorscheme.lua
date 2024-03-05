ca7c8bf273cedfa4840456afd550220ecb743bd9
Marcelo Perini Veloso <marcelo.veloso@streetbees.com>
2024-03-05 21:13:17 +0000

    install kickstart to nvim

--- config/nvim/lua/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd("colorscheme gruvbox")
-vim.cmd('let gruvbox="dark"')
-
-local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
-if not status then
-  print("Colorsheme not found!")
-  return
-end
